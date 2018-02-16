49d114391f58eda99d6f848f3c8febe6fb92c7ff
Constantinos Solomonides <Constantinos.Solomonides@citrix.com>
2018-02-17 01:21:09 +0200

    Code to handle alternating signs

--- number_calculator.py
+++ number_calculator.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 def calculator(input_argument):
+  if input_argument[0] < 0:
+    are_negatives = True 
+  else:
+    are_negatives = False
+
+  for number in input_argument:
+    if number < 0:
+      is_negative = True 
+    else:
+      is_negative = False
+    if not is_negative == are_negatives:
+      return None
+
   average = 1.0 * sum(input_argument) / len(input_argument)
   maximum = 1.0 * max(input_argument)
   return [average, maximum]
