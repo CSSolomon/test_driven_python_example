eb3b02d2f8649c6f0ec0a0511e55198fca500a3e
Constantinos Solomonides <Constantinos.Solomonides@citrix.com>
2018-02-17 01:12:00 +0200

    Second test to confirm functionality with all input

--- test_code.py
+++ test_code.py
@@ -9,4 +9,11 @@ class Test_Number_Calculator(unittest.TestCase):
     returned_result = number_calculator.calculator(input_argument)
     self.assertEquals(expected_result, returned_result)
 
+  def test_get_average_of_four_second_list(self):
+    input_argument  = [ 2, 4, 6, 8]
+    expected_result = [5, 8]
+    returned_result = number_calculator.calculator(input_argument)
+    self.assertEquals(expected_result, returned_result)
+    
+
 unittest.main()
