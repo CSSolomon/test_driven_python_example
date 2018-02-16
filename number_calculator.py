18cf2cf7084152e02d439fd8e774b2330f19dfc1
Constantinos Solomonides <Constantinos.Solomonides@citrix.com>
2018-02-17 01:14:07 +0200

    Code works OK for list

--- number_calculator.py
+++ number_calculator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 def calculator(input_argument):
-  average = sum(input_argument) / len(input_argument)
-  maximum = max(input_argument)
+  average = 1.0 * sum(input_argument) / len(input_argument)
+  maximum = 1.0 * max(input_argument)
   return [average, maximum]
