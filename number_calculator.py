d9042d690ff5c0fc06b00a6fe6f3dde0937d9c66
Constantinos Solomonides <Constantinos.Solomonides@citrix.com>
2018-02-17 02:43:47 +0200

    Test for string-to-number-list conversion function

--- number_calculator.py
+++ number_calculator.py
@@ -23,3 +23,6 @@ def calculator(input_argument):
   average = 1.0 * sum(input_argument) / len(input_argument)
   maximum = 1.0 * max(input_argument)
   return [average, maximum]
+
+def convert_string_to_list_of_numbers(input_string):
+  pass
--- test_code.py
+++ test_code.py
@@ -50,4 +50,12 @@ class Test_Number_Calculator(unittest.TestCase):
     with self.assertRaises(ZeroDivisionError):
       returned_result = number_calculator.calculator(input_argument)
 
+  def test_input_string_to_number_list(self):
+    input_argument  = "1 2 3 4"
+    expected_result = [1, 2, 3, 4]
+    returned_result = number_calculator.convert_string_to_list_of_numbers(
+        input_argument)
+    self.assertEquals(expected_result, returned_result)
+    
+
 unittest.main()
